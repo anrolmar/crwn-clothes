7498bc810cfb3f21d115d0e51b58694f9771cf1a
Angel Roldan <angel.roldan@sqli.com>
2021-09-30 09:14:20 +0200

    Added redux persist to the application

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
+import { persistStore } from 'redux-persist';
 import { logger } from 'redux-logger';
 import reducers from './index';
 
 const middlewares = [logger];
 
-const store = createStore(reducers, applyMiddleware(...middlewares));
-export default store;
+export const store = createStore(reducers, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
